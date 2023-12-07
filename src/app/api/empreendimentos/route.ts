80745d6f206ea14dec404cc9e2942534125d0181
meneses <vitormeneses87@gmail.com>
2023-12-07 11:10:31 -0300

    fix

--- src/app/api/empreendimentos/route.ts
+++ src/app/api/empreendimentos/route.ts
@@ -1,6 +1,8 @@
 import { NextResponse, NextRequest } from "next/server";
 import { GetEmpreendimentos } from "@/lib/controllers/empreendimentosController";
 
+export const dynamic = "force-dynamic";
+
 export async function GET(req: NextRequest) {
   const response = await GetEmpreendimentos();
 
