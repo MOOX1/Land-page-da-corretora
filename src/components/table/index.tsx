4c293a93c074a71ad676b428a5fd3e5c3ed9040b
meneses <vitormeneses87@gmail.com>
2023-12-07 10:46:06 -0300

    fix: build

--- src/components/table/index.tsx
+++ src/components/table/index.tsx
@@ -25,6 +25,7 @@ export default function Table({ onClick }: ITableProps) {
   const fetchEmpreendimentos = async () => {
     try {
       fetch("/api/empreendimentos", {
+        cache: "no-cache",
         next: {
           revalidate: 0,
         },
